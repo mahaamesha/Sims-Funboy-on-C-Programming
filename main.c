ee67c066ae8ed896c4f454d82108c4f55a5ebab2
Muhamad Fakhry Fernanda <50653283+fakhryalhusayn@users.noreply.github.com>
2020-02-02 23:57:20 +0700

    MuhamadFakhryFernanda - 10118096 - File Final

--- main.c
+++ main.c
@@ -1,32 +1,55 @@
 #include <stdio.h>
-#include "header.h"
+#include "head.h"
 
 int main() {
-    struct fanboy crFanboy;
-    struct cAct crAct;
-    struct date crDate;
-    struct time crTime;
+    int pil;
 
-    int *numChoice;
+    initAct(&act[0],"Sleep",0,0,-10,15,0, 30);
+    initAct(&act[1],"Eat",10,5,-5,20,-10, 15);
+    initAct(&act[2],"Bath",5,0,35,5,0, 5);
+    initAct(&act[3],"Sport",5,15,-15,15,-5,-20);
+    initAct(&act[4],"Work",-20,15,-20,-15,40,-25);
+    initAct(&act[5],"Hobby",20,25,-5,-5,-15,-10);
+    initAct(&act[6],"Pray",10,5,0,5,0,0);
+    initAct(&act[7],"Hangout",15,10,-10,-10,-20,-10);
+    initAct(&act[8],"Study",-15,5,-5,-10,0,-20);
+    initAct(&act[9],"Party",30,25,-10,5,-25,-15);
+    initAct(&act[10],"Zero",-5,-5,-5,-5,0,-5);
 
-    printf("STATS LIFE\n");
-    printf("\n");
+    statsAwal();    //inisiaisasi stats awal
 
-    printf("Input nama: ");
-    scanf(&*crFanboy.name);
-    printf("Game Start\n");
-    printf("Do what do you want, %s\n", &crFanboy.name);
-    printf("\n");
+    printf("START\n");
+    printf("What's your name? ");
+    scanf("%s", (crFanboy).name);
+    printf("\nHello, %s\n", (crFanboy).name);
+    printf("This is your initial condition.\n");
+    pStats();   //cetak kondisi awal
+    printf("\nGET YOUR FIRST 100_MONEY\n");    //objektif game
 
-    while (crFanboy.life != 0)
-    //printf("%s", &crFanboy.life);
-    {
-        prtAct(crFanboy.life);
-        scanf("%d", &*numChoice);
-        myChoice(*numChoice);
-        crFanboy.exp += (1 + (1 / *crFanboy.exp));
-        valid(crFanboy); //cek stats
-        prtStat();
+    while (crFanboy.life == 1) {
+    printf("----------------------------------\n");
+
+    //input pilihan (1 s.d 10)
+    pAct();     //print opsi activities
+    printf("\nWhat's next?");
+    scanf("%d", &pil);
+    if (pil == 1) Action(&crFanboy, &act[pil-1]);
+    else if (pil == 2) Action(&crFanboy, &act[pil-1]);
+    else if (pil == 3) Action(&crFanboy, &act[pil-1]);
+    else if (pil == 4) Action(&crFanboy, &act[pil-1]);
+    else if (pil == 5) Action(&crFanboy, &act[pil-1]);
+    else if (pil == 6) Action(&crFanboy, &act[pil-1]);
+    else if (pil == 7) Action(&crFanboy, &act[pil-1]);
+    else if (pil == 8) Action(&crFanboy, &act[pil-1]);
+    else if (pil == 9) Action(&crFanboy, &act[pil-1]);
+    else if (pil == 10) Action(&crFanboy, &act[pil-1]);
+    else Action(&crFanboy, &act[10]);
+
+    valid(&crFanboy);
+    printf("%s' Stats\n", crFanboy.name);
+    pStats();   //cetak kondisi akhir
+    //scanf("%d", crFanboy.life);
     }
+
     return 0;
 }
